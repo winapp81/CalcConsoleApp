516a92430bd66fb39e81d2b48c40d5484cb8cec3
박건태 <36683403+winapp81@users.noreply.github.com>
2018-10-14 23:52:16 +0900

    Add files via upload

---
+++ CalcSet.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcConsoleApp
+{
+    class CalcSet
+    {
+ 
+            //계산식
+        public string expr { set; get; }
+        public string resultVal { set; get; }
+        public string calcTime { set; get; }
+        public static List<string> exprList = new List<string>();
+
+        public CalcSet()
+        {
+            this.expr = "";
+            this.resultVal = "";
+            this.calcTime = "";
+        }
+
+        public CalcSet(string a, string b, string c)
+        {
+            this.expr = a;
+            this.resultVal = b;
+            this.calcTime = c;
+        }
+        
+        //소멸자
+        ~CalcSet()
+        {
+            this.expr = "";
+            this.resultVal = "";
+            this.calcTime = "";
+        }
+
+        public void AddCalcSet(string arg)
+        {
+            exprList.Add(arg);
+        }
+
+        public void CalculateExpr()
+        {
+            //연산자 우선순위별로, 루프진행
+            //1번 루프돌 때 마다, 원본 List 데이터 가공(지우고, 계산결과 넣기)
+            //계산시간 넣고, 결과값 넣기
+            int resultInt = 0;
+
+            List<int> tempList = HaveOperator("*");
+            
+            for (int i = 0; i < tempList.Count+1; i++)
+            {
+                if (tempList.Count > 0)
+                {
+                    var first = int.Parse(tempList[0].ToString()) - 1;
+                    var second = int.Parse(tempList[0].ToString()) + 1;
+
+                    var one = int.Parse(exprList[first].ToString());
+                    var two = int.Parse(exprList[second].ToString());
+
+                    resultInt = (one * two);
+
+                    //exprList.RemoveRange(0, 3);
+                    //좌표에 있는 값을 날려줘야됨 -> 아래와 같이 변경
+                    
+                    exprList.RemoveRange(first, 3);
+
+                    //넣을때 앞뒤 연산자 처리 필요?
+                    exprList.Insert(first, resultInt.ToString());
+                }
+
+                tempList = HaveOperator("*");
+                if (tempList.Count == 0)
+                    break;
+            }
+
+            tempList = HaveOperator("/");
+
+            for (int i = 0; i < tempList.Count + 1; i++)
+            {
+                if (tempList.Count > 0)
+                {
+                    var first = int.Parse(tempList[0].ToString()) - 1;
+                    var second = int.Parse(tempList[0].ToString()) + 1;
+
+                    var one = int.Parse(exprList[int.Parse(tempList[0].ToString()) - 1].ToString());
+                    var two = int.Parse(exprList[int.Parse(tempList[0].ToString()) + 1].ToString());
+                    resultInt = (one / two);
+
+                    exprList.RemoveRange(first, 3);
+                    exprList.Insert(first, resultInt.ToString());
+                }
+
+                tempList = HaveOperator("/");
+                if (tempList.Count == 0)
+                    break;
+            }
+
+            for (int i = 0; i < exprList.Count + 1; i++)
+            {
+                if (exprList.Count > 0)
+                {
+                    var one = int.Parse(exprList[0].ToString());
+                    var two = int.Parse(exprList[2].ToString());
+                    if(exprList[1].ToString() == "+")
+                    {
+                        resultInt = one + two;
+                    }else if(exprList[1].ToString() == "-")
+                    {
+                        resultInt = one - two;
+                    }
+                    exprList.RemoveRange(0, 3);
+                    exprList.Insert(0, resultInt.ToString());
+                }                
+                if (exprList.Count == 1)
+                    break;
+            }
+
+
+            this.resultVal = resultInt.ToString();
+        }
+
+        // 곱하기 포함되어 있다며,곱하기의 좌표를 리턴
+        public List<int> HaveOperator(string arg_operator)
+        {
+            List<int> intList = new List<int>();
+
+            if (exprList.Count > 2)
+            {
+                for(int i = 0; i< exprList.Count; i++)
+                {
+                    if(exprList[i].ToString() == arg_operator)
+                    {
+                        intList.Add(i);
+                    }
+                }
+                
+            }
+            return intList;
+
+        }
+        
+                
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CalcConsoleApp
+{
+    class Program
+    {
+        static List<CalcSet> calcsetList = new List<CalcSet>();
+        static CalcSet calset2 = new CalcSet();
+
+        static void Main(string[] args)
+        {
+            MainDisplay();
+
+
+        }//Main
+
+        public static void MainDisplay()
+        {
+            Console.Clear();
+            Console.WriteLine(" ");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("1) 이전계산식 전체조회");
+            Console.WriteLine(" ");
+            Console.WriteLine("2) 계산기");
+            Console.WriteLine(" ");
+            Console.WriteLine("3) 종료");
+            Console.WriteLine(" ");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("원하는 메뉴를 선택하세요.");
+
+            var inputStr = Console.ReadLine();
+
+            switch (inputStr)
+            {
+                case "1":
+                    ShowHistorY();
+                    break;
+                case "2":
+                    Calculate();
+                    break;
+                case "3":
+                    return;
+
+            }
+        }//MainDisplay()
+
+
+        public static void SecondDisplay(string arg)
+        {
+            string main_str = "";
+
+            Console.Clear();
+            Console.WriteLine(" 계산기 ");
+            Console.WriteLine("   ");
+            //Console.WriteLine("현재 입력 식 : {0} ", calset2.expr );
+            Console.WriteLine("   ");
+            Console.WriteLine(" 숫자 혹은 연산자(+,-,/,*)를 하나씩 입력 후 엔터를 입력하세요");
+            Console.WriteLine(" 마지막에 = 입력 후 엔터를 입력하시면 결과값을 확인하실 수 있습니다 ");
+
+            main_str = string.Format(" 입력 --> {0}", arg);
+            Console.WriteLine(main_str);
+
+        }
+
+        private static void Calculate()
+        {
+
+            string tempStr = "";
+            
+            while (true)
+            {
+
+                SecondDisplay(calset2.expr);
+                tempStr = Console.ReadLine();
+
+                if (tempStr != "=")
+                {
+                    switch (Validate(tempStr))
+                    {
+                    case 0:
+                    case 1:
+                    case 2:
+                        calset2.AddCalcSet(tempStr);
+                        calset2.expr += tempStr;
+                        break;
+                    case 3:
+                    //숫자+문자 조합시 넣지 말것.
+                        break;
+                    }
+                }
+                else
+                {
+                    //히스토리 넣기
+                    calcsetList.Add(calset2);
+
+                   //마지막이니 계산식 최종호출
+                    calset2.CalculateExpr();
+
+                    Console.Clear();
+
+                    Console.WriteLine("계산식 : {0} =", calset2.expr);
+                    Console.WriteLine("결과 : {0}", calset2.resultVal);
+                    Console.WriteLine("아무키나 입력하시면 뒤로 갑니다. ");
+
+                    //마지막에, dispose 해야됨.
+                    calset2.expr = "";
+                    calset2.resultVal = "";
+
+
+                    Console.ReadLine();
+
+                    MainDisplay();
+                }
+            }
+
+        }
+
+        private static int Validate(string input)
+        {
+            int rtnVal = 0;
+            
+            /*  리턴코드 기준
+             *  0 : 숫자/연산자/=
+             *  1 : 숫자OK
+             *  2 : 연산자
+             *  3 : 숫자+연산자 같이 적음
+             *  9 : 숫자가 아님
+
+            */
+
+            //숫자다
+            if(numericCheck(input))
+            {
+                rtnVal = 1;
+            }
+            else
+            {
+            //숫자가 아니다
+
+
+                //숫자+연산자조합 체크
+                if (input.Length > 1)
+                {
+                    rtnVal = 3;   
+                }else if (input == "+" || input == "-" || input == "*" || input == "/")
+                {
+                    //연산자 Only
+                    rtnVal = 2;
+                }
+            }
+
+            return rtnVal;
+        }
+
+        //숫자여부 체크
+        private static bool numericCheck(string strNumber)
+        {
+            try
+            {
+                int iNumber = Convert.ToInt32(strNumber);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        private static void ShowHistorY()
+        {
+            Console.Clear();
+            Console.WriteLine("   ");
+            Console.WriteLine("이전 계산식 조회");
+            Console.WriteLine("   ");
+
+            if (calcsetList.Count > 0)
+            {
+                for (int i = 0; i < calcsetList.Count; i++)
+                {
+
+                    //구현필요
+                    //Console.WriteLine(calcsetList.ToString());
+                    CalcSet temp = calcsetList[i];
+                    Console.WriteLine("{0}번 입력값 및 결과 >>>  {1} = {2} ", i.ToString(), temp.expr, temp.resultVal);
+                    Console.WriteLine(" ");
+                }
+            }
+            else 
+            {
+                Console.WriteLine(" No Data....");
+                Console.WriteLine(" 뒤로 가려면 아무키나 누르세요!");
+                var input = Console.ReadLine();
+
+                MainDisplay();
+
+                //if(input == "X" || input == "x")
+                //{
+                //    MainDisplay();
+                //}
+                //else
+                //{
+                //    Console.Clear();
+                //    Console.WriteLine("잘못 누르셨습니다. 뒤로가려면 X");                    
+                //    var input3 = Console.ReadLine();
+                //}
+            }
+        }
+
+
+    }//class
+
+}//namespace
